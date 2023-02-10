bf151af185577792d5ed0bf76c4fdac3d8bde6bb
Jongyub Lee <duqlek123@naver.com>
2023-02-10 15:39:08 +0900

    World In Album
    1. 메인액티비티 수정
    - 네비게이션용 menu item 추가
    - 하단 네비게이션 추가
    - 조회, 픽 프래그먼트와 네비게이션 연동

---
+++ app/src/main/java/com/example/worldinalbum/activities/IntroActivity.kt
@@ -0,0 +1,12 @@
+package com.example.worldinalbum.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.worldinalbum.R
+
+class IntroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+    }
+}
